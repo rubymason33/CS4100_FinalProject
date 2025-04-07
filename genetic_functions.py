22e7a85c006e2fa979c31d810cd25890a91f117c
annika6246 <annikasalpukas@gmail.com>
2025-04-07 11:48:34 -0400

    genetic updates

--- genetic_functions.py
+++ genetic_functions.py
@@ -12,14 +12,134 @@ def col_conflicts(arr):
     return np.sum(9 - np.apply_along_axis(lambda col: len(np.unique(col)), axis=0, arr=arr))
 
 
-def row_sum_mse(arr):
-    return np.mean((np.sum(arr, axis=1) - np.sum(np.arange(1, 10)))**2)
+def box_conflicts(arr):
+    conflicts = 0
+    for i in range(3):
+        for j in range(3):
+            block = arr[i*3:(i+1)*3, j*3:(j+1)*3].flatten()
+            conflicts += 9 - len(np.unique(block))
+    return conflicts
 
 
-def col_sum_mse(arr):
-    return np.mean((np.sum(arr, axis=0) - np.sum(np.arange(1, 10))) ** 2)
+def total_conflicts(arr):
+    return row_conflicts(arr) + col_conflicts(arr) + box_conflicts(arr)
+
 
 # --------------------- Agent functions ---------------------
 
 # ideas: randomly swap any two values, randomly change a value,
 # replace duplicate value with random number, if row/col sum > correct --> np.min(rand_idx, rand_idx-1)
+
+
+def greedy_swap(arr, mutable):
+    for i in range(10):
+        num_conflicts = total_conflicts(arr)
+        idx1, idx2 = [mutable[idx] for idx in np.random.choice(len(mutable), 2, replace=False)]
+        arr[idx1], arr[idx2] = arr[idx2], arr[idx1]
+        if total_conflicts(arr) > num_conflicts:
+            rnd = np.random.randn()
+            if rnd < 0.9:
+                arr[idx1], arr[idx2] = arr[idx2], arr[idx1]
+    return arr
+
+
+def greedy_change(arr, mutable):
+    for i in range(10):
+        num_conflicts = total_conflicts(arr)
+        idx = mutable[np.random.choice(len(mutable), 1).item()]
+        original = arr[idx]
+        arr[idx] = np.random.randint(1, 10)
+        if total_conflicts(arr) > num_conflicts:
+            rnd = np.random.randn()
+            if rnd < 0.9:
+                arr[idx] = original
+    return arr
+
+
+def random_swap(arr, mutable):
+    idx1, idx2 = [mutable[idx] for idx in np.random.choice(len(mutable), 2, replace=False)]
+    arr[idx1], arr[idx2] = arr[idx2], arr[idx1]
+    return arr
+
+
+def random_change(arr, mutable):
+    idx = mutable[np.random.choice(len(mutable), 1).item()]
+    arr[idx] = np.random.randint(1, 10)
+    return arr
+
+
+def replace_dup_rows(arr, mutable):
+    for row_idx in range(9):
+        row = arr[row_idx]
+        row_mutables = np.array([col for (r, col) in mutable if r == row_idx])
+        included, counts = np.unique(row, return_counts=True)
+        if len(included) == 9:
+            continue
+
+        duplicates = included[counts > 1]
+        dup_mutable_idx = row_mutables[np.isin(row[row_mutables], duplicates)]
+
+        duplicate_idx = np.random.choice(dup_mutable_idx)
+        excluded_val = np.random.choice(np.setdiff1d(np.arange(1, 10), included))
+
+        arr[row_idx, duplicate_idx] = excluded_val
+    return arr
+
+
+def replace_dup_cols(arr, mutable):
+    for col_idx in range(9):
+        col = arr[:, col_idx]
+        col_mutables = np.array([row for (row, c) in mutable if c == col_idx])
+        included, counts = np.unique(col, return_counts=True)
+
+        if len(included) == 9:
+            continue
+
+        duplicates = included[counts > 1]
+        dup_mutable_idx = col_mutables[np.isin(col[col_mutables], duplicates)]
+
+        duplicate_idx = np.random.choice(dup_mutable_idx)
+        excluded_val = np.random.choice(np.setdiff1d(np.arange(1, 10), included))
+
+        arr[duplicate_idx, col_idx] = excluded_val
+    return arr
+
+
+def replace_dup_boxes(arr, mutable):
+    for box_row in range(3):
+        for box_col in range(3):
+            box_start_row, box_start_col = box_row * 3, box_col * 3
+            box_values = arr[box_start_row:box_start_row + 3, box_start_col:box_start_col + 3]
+
+            box_mutables = [(r, c) for (r, c) in mutable if
+                            box_start_row <= r < box_start_row + 3 and box_start_col <= c < box_start_col + 3]
+
+            included, counts = np.unique(box_values, return_counts=True)
+            if len(included) == 9:
+                continue
+
+            duplicates = included[counts > 1]
+            dup_mutable_idx = [(r, c) for (r, c) in box_mutables if arr[r, c] in duplicates]
+
+            duplicate_idx = dup_mutable_idx[np.random.choice(len(dup_mutable_idx))]
+            excluded_val = np.random.choice(np.setdiff1d(np.arange(1, 10), included))
+
+            arr[duplicate_idx] = excluded_val
+
+    return arr
+
+
+def shuffle_boxes(arr, mutable):
+    for i in range(3):
+        for j in range(3):
+            box_indices = [(r, c) for r in range(i*3, (i+1)*3)
+                                 for c in range(j*3, (j+1)*3)
+                                 if (r, c) in mutable]
+            values = [arr[r, c] for (r, c) in box_indices]
+            if len(values) > 1:
+                np.random.shuffle(values)
+                for (r, c), val in zip(box_indices, values):
+                    arr[r, c] = val
+    return arr
+
+# --------------------------------------------------------------------------------
--- sudoku_genetic.py
+++ sudoku_genetic.py
@@ -1,17 +1,108 @@
 import sudoku_tools as sutils
+import numpy as np
+from functools import reduce
+import copy
+import time
 
 
 class SudokuGeneticAlgorithm:
 
     def __init__(self, initial_board):
         self.initial_board = initial_board
-        self.mutable = []   # list of indices of mutable positions
-        self.population = {}    # potential solution array
+        self.mutable = list(zip(*np.where(self.initial_board == 0)))   # list of indices of mutable positions
+        self.population = {}    # potential solution array, scores: board
         self.fitness = {}   # name: func
         self.agents = {}    # name: func
 
     def add_fitness(self, func_name, func):
-        pass
+        self.fitness[func_name] = func
 
     def add_agent(self, func_name, func):
-        pass
+        self.agents[func_name] = func
+
+    def add_sample(self, sample):
+        ''' Evaluate fitness of sample and add to population'''
+        scores = tuple([(key, func(sample)) for key, func in self.fitness.items()])
+        self.population[scores] = sample
+
+    def get_sample(self):
+        ''' Get random sample from current population '''
+        idx = np.random.choice(len(self.population.values()))
+        sample = list(self.population.values())[idx]
+        return copy.deepcopy(sample)
+
+    def get_best_solution(self):
+        total_scores = [key[3][1] for key in self.population.keys()]
+        best_eval, best_sol = list(self.population.items())[np.argmin(total_scores)]
+        return best_eval, best_sol
+
+    @staticmethod
+    def _dominates(p, q):
+        ''' Checks whether one solution (p) dominates another (q) '''
+        score_diffs = [y - x for (_, x), (_, y) in zip(p, q)]
+        return min(score_diffs) >= 0.0 and max(score_diffs) > 0.0
+
+    @staticmethod
+    def _reduce_population(solutions, p):
+        return solutions - {q for q in solutions if SudokuGeneticAlgorithm._dominates(p, q)}
+
+    def remove_dominated(self):
+        non_dominated = reduce(SudokuGeneticAlgorithm._reduce_population,
+                               self.population.keys(), self.population.keys())
+        self.population = {key: self.population[key] for key in non_dominated}
+
+    def mutate(self, agent_name):
+        ''' Mutate a random solution in current population with agent '''
+        agent_func = self.agents[agent_name]
+        sample = self.get_sample()
+        mutated = agent_func(sample, self.mutable)
+        self.add_sample(mutated)
+
+    def crossover(self, parent1, parent2):
+        ''' Crossover population '''
+        crossover_point = np.random.randint(0, 9)
+        offspring = np.vstack((parent1[:crossover_point], parent2[crossover_point:]))
+
+        # agent_keys = list(self.agents.keys())
+        # selected_agent = np.random.choice(agent_keys)
+        # agent_func = self.agents[selected_agent]
+        # mutated = agent_func(offspring, self.mutable)
+        self.add_sample(offspring)
+
+    def evolve(self, num_iter=100000, selection_period=150, crossover_point=200, time_limit=600):
+        ''' Evolve population '''
+        start_time = time.time()
+        agent_keys = list(self.agents.keys())
+
+        for i in range(num_iter):
+            if time.time() - start_time >= time_limit:
+                break
+
+            selected_agent = np.random.choice(agent_keys)
+            if i > 5000:
+                rnd = np.random.randn()
+                if rnd < 0.5:
+                    selected_agent = 'shuffle boxes'
+
+            self.mutate(selected_agent)
+
+            if i % crossover_point == 0:
+                for _ in range(50):
+                    parent1 = self.get_sample()
+                    parent2 = self.get_sample()
+                    self.crossover(parent1, parent2)
+
+            if i > 0 and i % selection_period == 0:
+                self.remove_dominated()
+                best_eval, best_sol = self.get_best_solution()
+                if best_eval[3][1] == 0:
+                    break
+                if i % 1000 == 0:
+                    print(best_eval)
+
+            if i % 1000 == 0:
+                print(f'{i}/{num_iter}')
+                print(f'Population size: {len(self.population)}')
+
+        print('Evolution finished')
+        self.remove_dominated()
